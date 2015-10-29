7700e9bbbc68bc411f99c86b5acc59e53c86407f
Andy Ganchrow <andy.addington@ganchrow.com>
2015-10-29 16:51:57 -0700

    Allow reload listener

--- lib/redis_startup.rb
+++ lib/redis_startup.rb
@@ -12,42 +12,58 @@ class RedisStartup
 
   def initialize(client, opts, &blk)
     @client = client
-    @log = opts[:log]
-    @result_key = opts[:result_key] || 'results'
+    @log = opts.delete(:log)
+    @proc = opts.delete(:cb) || opts.delete(:proc)
+    @result_key = opts.delete(:result_key) || 'result'
     @callback = blk
-    post_init(opts) unless opts.empty?
+    @default_cb = Array(opts.delete(:default_cb) || [])
+    post_init(@opts = opts) unless opts.empty?
   end
 
   def post_init(opts)
     @close_on_done = opts.delete(:close_on_done)
-    EM::Iterator.new(opts).each(proc do |(m, key), iter|
-      if(key.is_a?(Array))
-	_iter_object(m, key, iter)
-      elsif key.is_a?(Hash) && key.key?(:uri)
-	_spawn(key, iter)
-      else
-	_iter_list(m, key, iter)
-      end
-    end, proc { |*| _try_close; @callback.call if @callback.respond_to?(:call) })
+    @proc = method(:_method) unless @proc.respond_to?(:call)
+    @persist = opts.delete(:persist) unless @close_on_done 
+    EM::Iterator.new(opts).each(
+      method(:_proc).to_proc,
+      proc { |*| _try_close; @callback.call if @callback.respond_to?(:call) }
+    )
   end
 
   private
 
+  def _proc((m, key), iter = nil)
+    return iter.try(:next) unless key
+    if(key.is_a?(Array))
+      _iter_object(m, key, iter)
+    elsif key.is_a?(Hash) && (key.key?(:redis) || key.key?(:uri))
+      _spawn(key, iter)
+    else
+      _iter_list(m, key, iter)
+    end
+  end
+
   def _try_close
-    @close_on_done && @client.close_connection
+    if @close_on_done
+      @client.close_connection
+    else
+      @client.pubsub.subscribe(@persist) do |m|
+        _proc([m, @opts[m.to_sym]]) if m && @opts[m.to_sym]
+      end if @persist
+    end 
   end
 
   def _spawn(opts, iter)
     new_client = EM::Hiredis.connect(opts.delete(:uri))
     new_client.callback do |*|
-      self.class.new(new_client, opts) { |*| iter.next }
+      self.class.new(new_client, opts.merge(result_key: @result_key, log: @log)) { |*| iter.next }
     end.errback do |e|
       @log.try(:debug, e)
       iter.next
     end
   end
 
-  def _data_warehouse_method(key, res)
+  def _method(key, res)
     MODULE.method(format(PREFIX, key).to_sym).call({@result_key => res})
   end
 
@@ -72,9 +88,13 @@ class RedisStartup
   end
 
   def _callback(m, res, iter)
-    _data_warehouse_method(m, res)
+    if @default_cb.member?(m)
+      _method(m, res)
+    else
+      @proc.call(m, res)
+    end
     @log.try(:debug, "Loaded #{m}")
-    iter.next
+    iter.next if iter
   end
 
   def _array_slice_to_hash(res)
